e742e4905f77ed3cb04038c61a1bea67b99cda91
bragamat <mateuscbraga@gmail.com>
2018-05-17 03:51:32 +0000

    sup

--- db/migrations/20180503213429_create_ratings.js
+++ db/migrations/20180503213429_create_ratings.js
@@ -4,7 +4,7 @@ exports.up = function(knex, Promise) {
     knex.schema.createTable('ratings', function(table){   
     table.increments();
     table.integer('rating');
-    table.integer('user_id').references('id').inTable('users_table');
+    // table.integer('user_id').references('id').inTable('users_table');
     })
   ])
 }
